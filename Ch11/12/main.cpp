cee2d00f74933a0c3e2e00aa049f6bb7f98cc856
guneyk98 <guneyk98@gmail.com>
2025-10-26 23:08:22 +0000

    fix solution for Ch11 Ex12

--- Ch11/12/main.cpp
+++ Ch11/12/main.cpp
@@ -7,7 +7,7 @@ using namespace Graph_lib;
 constexpr double pi = numbers::pi_v<double>;
 
 struct Right_triangle : Shape {
-	Right_triangle(Point p, int x, int y);
+	Right_triangle(Point p, double dx, double dy, double rot);
 	void draw_specifics(Painter& painter) const override;
 };
 
@@ -20,52 +20,53 @@ int main()
 	background_fill.set_fill_color(Color::white);
 	win.attach(background_fill);
 
-	constexpr int size = 100;
+	constexpr double tri_w = 100.0;
+	const double small_tri_w = std::sqrt(0.5*tri_w*tri_w);
+	const double pent_w = 2.0*small_tri_w + tri_w;
+
 	const Point c{win.x_max()/2, win.y_max()/2};
+	Mark m{c, 'x'};
+	win.attach(m);
 
-	const Point p1{c.x+size/2, c.y-size/2};
+	const int len_large = std::lround(pent_w/2.0);
+	const int len_small = std::lround(tri_w/2.0);
 
-	Right_triangle t1{{p1.x, p1.y-size}, -size, size};
-	Right_triangle t2{p1, size, -size};
-	Right_triangle t3{{p1.x+size, p1.y}, -size, size};
-	
-	const Point p2{c.x-size/2, c.y+size/2};
+	//offset from centre to right-angles
+	std::array<std::array<int, 2>, 8> offsets{{
+		{+len_small, -len_large},
+		{+len_large, -len_small},
+		{+len_large, +len_small},
+		{+len_small, +len_large},
+		{-len_small, +len_large},
+		{-len_large, +len_small},
+		{-len_large, -len_small},
+		{-len_small, -len_large},
+	}};
 
-	Right_triangle t4{{p2.x-size, p2.y}, size, -size};
-	Right_triangle t5{p2, -size, size};
-	Right_triangle t6{{p2.x, p2.y+size}, size, -size};
+	double theta = pi/2.0;
+	int colour_index = 0;
+	Vector_ref<Right_triangle> tris;
+	for (int i = 0; i < offsets.size(); ++i) {
+		const int x = c.x + offsets[i][0];
+		const int y = c.y + offsets[i][1];
+		tris.push_back(make_unique<Right_triangle>(Point{x, y}, tri_w, tri_w, theta));
+		tris[tris.size()-1].set_fill_color(colour_index);
+		win.attach(tris[tris.size()-1]);
 
-	const Point p3{c.x-size/2, c.y-size/2};
-	Right_triangle t7{p3, -size, -size};
+		++colour_index;
+		theta += pi/4.0;
+	}
 
-	const Point p4{c.x+size/2, c.y+size/2};
-	Right_triangle t8{p4, size, size};
-
-	t1.set_fill_color(Color::black);
-	t2.set_fill_color(Color::blue);
-	t3.set_fill_color(Color::cyan);
-	t4.set_fill_color(Color::red);
-	t5.set_fill_color(Color::green);
-	t6.set_fill_color(Color::yellow);
-	t7.set_fill_color(Color::magenta);
-	t8.set_fill_color(Color::dark_red);
-
-	win.attach(t1);
-	win.attach(t2);
-	win.attach(t3);
-	win.attach(t4);
-	win.attach(t5);
-	win.attach(t6);
-	win.attach(t7);
-	win.attach(t8);
 	win.wait_for_button();
 }
 
-Right_triangle::Right_triangle(Point p, int dx, int dy)
+Right_triangle::Right_triangle(Point p, double dx, double dy, double rot)
 {
 	add(p);
-	add({p.x+dx, p.y});
-	add({p.x, p.y+dy});
+	const double cost = std::cos(rot);
+	const double sint = std::sin(rot);
+	add({p.x + std::lround(dx*cost-sint), p.y + std::lround(dx*sint+cost)});
+	add({p.x + std::lround(cost-dy*sint), p.y + std::lround(sint+dy*cost)});
 }
 
 void Right_triangle::draw_specifics(Painter& painter) const
