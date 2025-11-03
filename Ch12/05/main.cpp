de8c8eb28fe85cc8347c6b8453b0898130d911db
guneyk98 <guneyk98@gmail.com>
2025-11-03 16:55:23 +0000

    Ch12 Ex05

---
+++ Ch12/05/main.cpp
@@ -0,0 +1,52 @@
+#include "PPP/Simple_window.h"
+#include "PPP/Graph.h"
+
+using namespace Graph_lib;
+
+struct Striped_rectangle : Rectangle {
+	Striped_rectangle(Point xy, int ww, int hh);
+	void set_fill_color(Color col) { hlines.set_color(col); redraw(); }
+	virtual void draw_specifics(Painter& painter) const override;
+	virtual void move(int dx, int dy) override;
+private:
+	Lines hlines;
+};
+
+int main()
+{
+	Application app;
+	Simple_window win(Point{50, 50}, 800, 800, "Ch12 Ex05");
+	//window is dark by default (OS dark-mode?), fill it white
+	Rectangle background_fill{Point{0, 0}, win.x_max(), win.y_max()};
+	background_fill.set_fill_color(Color::white);
+	win.attach(background_fill);
+
+	Striped_rectangle r{{100, 100}, 300, 200};
+	win.attach(r);
+	win.wait_for_button();
+	r.set_fill_color(Color::red);
+	win.wait_for_button();
+}
+
+Striped_rectangle::Striped_rectangle(Point xy, int ww, int hh)
+	: Rectangle{xy, ww, hh}
+{
+	hlines.set_color(Color::invisible);
+	const int x1 = xy.x+1;
+	const int x2 = xy.x + ww-1;
+	for (int y = xy.y+1; y < xy.y + hh; y+=2) {
+		hlines.add({x1, y}, {x2, y});
+	}
+}
+
+void Striped_rectangle::draw_specifics(Painter& painter) const
+{
+	Rectangle::draw_specifics(painter);
+	hlines.draw(painter); //needs to be draw() for the lines to use their own colour
+}
+
+void Striped_rectangle::move(int dx, int dy)
+{
+	Rectangle::move(dx, dy);
+	hlines.move(dx, dy);
+}
