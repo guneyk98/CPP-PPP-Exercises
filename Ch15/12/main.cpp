11d95d5070c44070b4077a69deab59728dbe5e28
guneyk98 <guneyk98@gmail.com>
2025-12-04 16:01:37 +0000

    finish exercise

--- Ch15/12/main.cpp
+++ Ch15/12/main.cpp
@@ -24,16 +24,47 @@ struct Link {
 Link* insert(Link* p, Link* n); //insert before an element
 Link* add(Link* p, Link* n); //insert after an element
 Link* erase(Link* p); //remove an element
-Link* find(Link* p, const God& g); //find a link with a given value
+Link* find(Link* p, const std::string& s); //find a link with a given value
 Link* advance(Link* p, int n); //get the nth successor
+Link* move(Link* p, Link* new_list, Link** old_list); //move element to a new list, returns the new list
+
 void print_all(Link* p);
 
 int main()
 {
 	Link* gods = new Link{{"Thor", "Norse", "", "Mjolnir"}};
 	gods = gods->add_ordered(new Link{{"Odin", "Norse", "Sleipner", "Gungnir"}});
-	gods = gods->add_ordered(new Link{{"Zeus", "Greek", "", "lightning"}});
+	gods = gods->add_ordered(new Link{{"Lugh", "Irish", "Enbarr", "Gae Assail"}});
+	gods = gods->add_ordered(new Link{{"Zeus", "Greek", "", "Thunderbolt"}});
+	gods = gods->add_ordered(new Link{{"Poseidon", "Greek", "Pegasus", "Trident"}});
+	gods = gods->add_ordered(new Link{{"The Dagda", "Irish", "", "Lorg Mor"}});
+	gods = gods->add_ordered(new Link{{"Freyr", "Norse", "Blodughofi", "Freyr's Sword"}});
+	gods = gods->add_ordered(new Link{{"Ares", "Greek", "Aithon, Phlogios, Konabos and Phobos", "Spear"}});
+	gods = gods->add_ordered(new Link{{"The Morrigan", "Irish", "", ""}});
+
+	std::cout << "########################## Gods ##########################\n";
 	print_all(gods);
+
+	Link* greek_gods = move(find(gods, "Ares"), nullptr, &gods);
+	greek_gods = move(find(gods, "Zeus"), greek_gods, &gods);
+	greek_gods = move(find(gods, "Poseidon"), greek_gods, &gods);
+
+	Link* norse_gods = move(find(gods, "Odin"), nullptr, &gods);
+	norse_gods = move(find(gods, "Thor"), norse_gods, &gods);
+	norse_gods = move(find(gods, "Freyr"), norse_gods, &gods);
+
+	Link* irish_gods = move(find(gods, "The Morrigan"), nullptr, &gods);
+	irish_gods = move(find(gods, "The Dagda"), irish_gods, &gods);
+	irish_gods = move(find(gods, "Lugh"), irish_gods, &gods);
+
+	std::cout << "\n####################### Greek Gods #######################\n";
+	print_all(greek_gods);
+
+	std::cout << "\n####################### Norse Gods #######################\n";
+	print_all(norse_gods);
+
+	std::cout << "\n####################### Irish Gods #######################\n";
+	print_all(irish_gods);
 }
 
 Link* insert(Link* p, Link* n)
@@ -75,10 +106,10 @@ Link* erase(Link* p)
 	return p->succ;
 }
 
-Link* find(Link* p, const God& g)
+Link* find(Link* p, const std::string& s)
 {
 	while (p) {
-		if (p->value == g)
+		if (p->value.name == s)
 			return p;
 		p = p->succ;
 	}
@@ -128,4 +159,18 @@ Link* Link::add_ordered(Link* p)
 
 	add(curr, p);
 	return this;
+}
+
+Link* move(Link* p, Link* new_list, Link** old_list)
+{
+	if (p == nullptr) //if find fails
+		return new_list;
+	erase(p);
+	if (p == *old_list) //if moving the first element of the old list
+		*old_list = (*old_list)->succ;
+	p->prev = nullptr;
+	p->succ = nullptr;
+	if (new_list == nullptr)
+		return p;
+	return new_list->add_ordered(p);
 }
