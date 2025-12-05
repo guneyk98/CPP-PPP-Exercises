8c79929a27019636d43955ec7911fba2e7a1cb62
guneyk98 <guneyk98@gmail.com>
2025-12-05 09:27:57 +0000

    Ch16 Ex01

---
+++ Ch16/01/main.cpp
@@ -0,0 +1,15 @@
+import std;
+
+void to_lower(char* s)
+{
+	for (; *s; ++s)
+		if (*s >= 'A' && *s <= 'Z')
+			*s -= 'A'-'a';
+}
+
+int main()
+{
+	char str[] = "Hello World!";
+	to_lower(str);
+	std::cout << str << '\n';
+}
